e4e9a07591c8fea24117120858debdaf23dcd43e
David Bakin <david@bakins-bits.com>
2015-04-14 23:59:09 -0700

    Make the ILMerge NuGet packages be development dependencies so they don't become dependencies of the NuGet package I'm uploading.

---
+++ CommandLineLexing/Scripts/make-nuget-packages.cmd
@@ -0,0 +1,2 @@
+rem nuget pack CommandLineLexing.csproj -Verbosity detailed -Build -Symbols -Properties Configuration=Release
+nuget pack CommandLineLexing.csproj -Verbosity detailed -Build -Symbols -IncludeReferencedProjects -Properties Configuration=Release
---
+++ CommandLineLexing/Scripts/push-nuget-packages.cmd
@@ -0,0 +1,1 @@
+nuget push BakinsBits.CommandLineLexing.0.5.0-beta02.nupkg -Verbosity detailed
